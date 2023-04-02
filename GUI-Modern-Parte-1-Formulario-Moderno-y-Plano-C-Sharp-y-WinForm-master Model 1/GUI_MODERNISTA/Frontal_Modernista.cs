35201cdbe467edc12e029c179124fae49332b2e6
Brayan JC <87408230+BrayanJC@users.noreply.github.com>
2023-04-02 17:50:57 -0500

    Commit

---
+++ GUI-Modern-Parte-1-Formulario-Moderno-y-Plano-C-Sharp-y-WinForm-master Model 1/GUI_MODERNISTA/Frontal_Modernista.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace GUI_MODERNISTA
+{
+    public partial class Frontal_Modernista : Form
+    {
+        public Frontal_Modernista()
+        {
+            InitializeComponent();
+        }
+        int lx, ly;
+        int sw, sh;
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnMaximizar_Click(object sender, EventArgs e)
+        {
+            lx = this.Location.X;
+            ly = this.Location.Y;
+            sw = this.Size.Width;
+            sh = this.Size.Height;
+            btnMaximizar.Visible = false;
+            btnRestaurar.Visible = true;
+            this.Size = Screen.PrimaryScreen.WorkingArea.Size;
+            this.Location = Screen.PrimaryScreen.WorkingArea.Location;
+        }
+
+        private void btnRestaurar_Click(object sender, EventArgs e)
+        {
+            btnMaximizar.Visible = true;
+            btnRestaurar.Visible = false;
+            this.Size = new Size(sw, sh);
+            this.Location = new Point(lx, ly);
+        }
+
+        private void btnMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+        private void BarraTitulo_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void btnReportes_Click(object sender, EventArgs e)
+        {
+            SubmenuReportes.Visible = true;
+        }
+
+        private void btnrptventa_Click(object sender, EventArgs e)
+        {
+            SubmenuReportes.Visible = false;
+        }
+
+        private void btnrptcompra_Click(object sender, EventArgs e)
+        {
+            SubmenuReportes.Visible = false;
+        }
+
+        private void btnrptpagos_Click(object sender, EventArgs e)
+        {
+            SubmenuReportes.Visible = false;
+        }
+
+        private void btnsalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+       
+    }
+}
